c095e4ab11e77b3842127caf4e9edc6b76e1573e
Rizwan Reza <riz@hey.com>
2025-03-07 23:14:57 -0600

    recipe completed

--- chapter-2/recipe-5/shop_smart/app/models/order.rb
+++ chapter-2/recipe-5/shop_smart/app/models/order.rb
@@ -3,4 +3,24 @@ class Order < ApplicationRecord
 
   validates :total_price, presence: true, numericality: {greater_than_or_equal_to: 0}
   validates :status, presence: true
+
+  before_validation :set_default_status, unless: ->(order) { order.status.present? }
+  after_create ->(order) { InvoiceService.generate(order) }
+  after_commit :send_confirmation_email
+  after_save :give_loyalty_points
+
+  private
+
+  def set_default_status
+    self.status = "pending"
+  end
+
+  def send_confirmation_email
+    OrderMailer.with(order: self).confirmation_email.deliver_later
+  end
+
+  def give_loyalty_points
+    points_awarded = (total_price / 10).to_i
+    customer.update(loyalty_points: customer.loyalty_points + points_awarded)
+  end
 end
